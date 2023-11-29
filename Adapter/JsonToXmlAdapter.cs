6a323fbcc928c5062de1a79874c786ad3ba5762e
Prykhodchenko Oleksandra <119930416+Lesbica@users.noreply.github.com>
2023-11-29 15:29:38 +0200

    фппявппып

--- Adapter/JsonToXmlAdapter.cs
+++ Adapter/JsonToXmlAdapter.cs
@@ -1,5 +1,7 @@
-﻿using System.Xml.Linq;
+﻿using System.Xml;
+using System.Xml.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Adapter
 {
@@ -9,13 +11,70 @@ namespace Adapter
         {
             try
             {
-                var xmlData = JsonConvert.DeserializeXNode(jsonData, "Root");
-                return xmlData.ToString();
+                XmlDocument xmlDoc = new XmlDocument();
+                XmlNode rootNode = JsonConvertToXmlNode(jsonData, xmlDoc);
+                xmlDoc.AppendChild(rootNode);
+
+                return xmlDoc.OuterXml;
             }
             catch (Exception ex)
             {
                 return $"Error converting JSON to XML: {ex.Message}";
             }
+        }
+
+        private XmlNode JsonConvertToXmlNode(string jsonData, XmlDocument xmlDoc)
+        {
+            var jsonObject = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(jsonData);
+            return ConvertJsonToXmlNode(jsonObject, xmlDoc);
+        }
+
+        private XmlNode ConvertJsonToXmlNode(Newtonsoft.Json.Linq.JObject jsonObject, XmlDocument xmlDoc)
+        {
+            var rootElement = xmlDoc.CreateElement("Root");
+
+            foreach (var property in jsonObject.Properties())
+            {
+                var childNode = ConvertJsonToXmlNode(property.Name, property.Value, xmlDoc);
+                rootElement.AppendChild(childNode);
+            }
+
+            return rootElement;
+        }
+
+        private XmlNode ConvertJsonToXmlNode(string nodeName, Newtonsoft.Json.Linq.JToken token, XmlDocument xmlDoc)
+        {
+            var node = xmlDoc.CreateElement(nodeName);
+
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    var jsonObject = (Newtonsoft.Json.Linq.JObject)token;
+                    foreach (var property in jsonObject.Properties())
+                    {
+                        var childNode = ConvertJsonToXmlNode(property.Name, property.Value, xmlDoc);
+                        node.AppendChild(childNode);
+                    }
+                    break;
+
+                case JTokenType.Array:
+                    var jsonArray = (Newtonsoft.Json.Linq.JArray)token;
+                    foreach (var arrayItem in jsonArray)
+                    {
+                        var childNode = ConvertJsonToXmlNode("Item", arrayItem, xmlDoc);
+                        node.AppendChild(childNode);
+                    }
+                    break;
+
+                case JTokenType.Null:
+                    break;
+
+                default:
+                    node.InnerText = token.ToString();
+                    break;
+            }
+
+            return node;
         }
     }
 }
---
+++ Adapter/XmlToJsonAdapter.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+namespace Adapter
+{
+    public class XmlToJsonAdapter : IDataAdapter
+    {
+        public string ConvertData(string xmlData)
+        {
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xmlData);
+
+                StringBuilder jsonBuilder = new StringBuilder();
+                ConvertXmlNodeToJson(xmlDoc.DocumentElement, jsonBuilder);
+
+                return jsonBuilder.ToString();
+            }
+            catch (Exception ex)
+            {
+                return $"Error converting XML to JSON: {ex.Message}";
+            }
+        }
+
+        private void ConvertXmlNodeToJson(XmlNode node, StringBuilder jsonBuilder)
+        {
+            switch (node.NodeType)
+            {
+                case XmlNodeType.Element:
+                    jsonBuilder.Append("{");
+                    jsonBuilder.Append($"\"{node.Name}\": ");
+                    if (node.HasChildNodes)
+                    {
+                        jsonBuilder.Append("[");
+                        foreach (XmlNode childNode in node.ChildNodes)
+                        {
+                            ConvertXmlNodeToJson(childNode, jsonBuilder);
+                            jsonBuilder.Append(",");
+                        }
+                        jsonBuilder.Remove(jsonBuilder.Length - 1, 1); // Remove trailing comma
+                        jsonBuilder.Append("]");
+                    }
+                    else
+                    {
+                        jsonBuilder.Append("null");
+                    }
+                    jsonBuilder.Append("}");
+                    break;
+                case XmlNodeType.Text:
+                    jsonBuilder.Append($"\"{node.InnerText}\"");
+                    break;
+            }
+        }
+    }
+}
